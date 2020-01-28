50c058d1dc56be632afa5d18fd7175716bd20617
Kailash <53666209+kailash230890@users.noreply.github.com>
2020-01-28 22:46:40 +0530

    Add files via upload

---
+++ src/components/About.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="container">
+      <h4 className="center">About</h4>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+        officiis blanditiis temporibus magnam tempora id veritatis autem libero
+        itaque, numquam nulla iste maxime laudantium provident corporis, optio
+        molestias ipsam ratione omnis ab? Recusandae quae animi adipisci vitae!
+        Ipsum consequuntur harum assumenda quos eius enim sequi provident est,
+        quas velit corporis. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Deleniti officiis blanditiis temporibus magnam tempora id
+        veritatis autem libero itaque, numquam nulla iste maxime laudantium
+        provident corporis, optio molestias ipsam ratione omnis ab? Recusandae
+        quae animi adipisci vitae! Ipsum consequuntur harum assumenda quos eius
+        enim sequi provident est, quas velit corporis.
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="container">
+      <h4 className="center">Contact</h4>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+        officiis blanditiis temporibus magnam tempora id veritatis autem libero
+        itaque, numquam nulla iste maxime laudantium provident corporis, optio
+        molestias ipsam ratione omnis ab? Recusandae quae animi adipisci vitae!
+        Ipsum consequuntur harum assumenda quos eius enim sequi provident est,
+        quas velit corporis. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Deleniti officiis blanditiis temporibus magnam tempora id
+        veritatis autem libero itaque, numquam nulla iste maxime laudantium
+        provident corporis, optio molestias ipsam ratione omnis ab? Recusandae
+        quae animi adipisci vitae! Ipsum consequuntur harum assumenda quos eius
+        enim sequi provident est, quas velit corporis. Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Deleniti officiis blanditiis
+        temporibus magnam tempora id veritatis autem libero itaque, numquam
+        nulla iste maxime laudantium provident corporis, optio molestias ipsam
+        ratione omnis ab? Recusandae quae animi adipisci vitae! Ipsum
+        consequuntur harum assumenda quos eius enim sequi provident est, quas
+        velit corporis.
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
